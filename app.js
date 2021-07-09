859fbae3e7c9443c58422f7b117db93c68a7a3fc
Kyle Watkins <kylewatkins1202@gmail.com>
2021-07-09 12:37:00 -0400

    HomeWork1

---
+++ app.js
@@ -0,0 +1,74 @@
+(function(){
+'use strict';
+
+angular.module('lunchCounter',[])
+
+.controller('lunchCounterController', function($scope){
+    
+    $scope.items="";
+    $scope.output="";
+
+    $scope.displayItemCount=function(){
+        
+        var itemCount=calculateItems($scope.items);
+        $scope.output = checkIfTooMuch(itemCount);
+    
+    }
+    
+    $scope.noSpace=function(event){
+        
+        if(event.keyCode == 32){
+            
+            event.preventDefault();
+        
+        }
+
+    }
+});
+
+function calculateItems(string){
+    
+    var itemArray = string.split(',');
+    var items=[]
+    
+    for(var i = 0; i < itemArray.length; i++)
+    {
+
+        itemArray[i]=itemArray[i].replaceAll(" ","")
+        
+        if(itemArray[i]!="")
+        {
+
+            items.push(itemArray[i])
+        
+        }
+
+    }
+    
+    return items.length;
+
+}
+
+function checkIfTooMuch(count)
+{
+
+    if(count==0)
+    {
+
+        return "Empty.";
+    
+    }
+    else if(count<=3)
+    {
+
+        return "Enjoy!";
+    
+    }
+    else{
+
+        return "That's too much.";
+    
+    }
+
+}
+})();
